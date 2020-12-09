62e886ee498a254d5007b993695e25fdafe0d505
Jacob Fisher <jf9260@bard.edu>
2020-12-09 18:15:22 +0000

    modals centered and fixed

--- js/modalCode.js
+++ js/modalCode.js
@@ -2,10 +2,42 @@
 var modal = document.getElementById("myModal");
 
 // get the image and insert it inside the modal
-var modalClick = document.getElementById("wigm");
-var vid01 = document.getElementById("vid0");
-modalClick.onclick = function() {
+// wigm, inv_sp, env_imp, psae;
+var wigm  = document.getElementById("wigm");
+var env_imp = document.getElementById("env_imp");
+var inv_sp = document.getElementById("inv_sp");
+var psae = document.getElementById("psae");
+
+var vid0 = document.getElementById("vid0");
+var vid1 = document.getElementById("vid1");
+var vid2 = document.getElementById("vid2");
+var vid3 = document.getElementById("vid3");
+
+var seeable_media;
+
+
+wigm.onclick = function() {
 	modal.style.display = "block";
+	seeable_media = vid0;
+	vid0.style.display="block";
+}
+
+env_imp.onclick = function() {
+	modal.style.display = "block";
+	seeable_media = vid1;
+	vid1.style.display="block";
+}
+
+inv_sp.onclick = function() {
+	modal.style.display = "block";
+	seeable_media = vid2;
+	vid2.style.display="block";
+}
+
+psae.onclick = function() { 
+	modal.style.display = "block";
+	seeable_media = vid3;
+	vid3.style.display = "block";
 }
 
 
@@ -15,4 +47,5 @@ var span = document.getElementsByClassName("close")[0];
 // when the user clicks on <span> (x), close the modal
 span.onclick = function() {
 	modal.style.display = "none";
+	seeable_media.style.display = "none";
 }
